39d28e0f1ebd1aa67d3493c6afe9d892a52655ad
H4ttiz <leonardocbizerra@gmail.com>
2026-03-21 22:47:16 -0300

    feat: adiciona service de Autores

---
+++ src/main/java/com/unicesumar/lista_cruds_springboot/autores/service/AutorService.java
@@ -0,0 +1,34 @@
+package com.unicesumar.lista_cruds_springboot.autores.service;
+
+import com.unicesumar.lista_cruds_springboot.autores.model.AutorModel;
+import com.unicesumar.lista_cruds_springboot.autores.repository.AutorRepository;
+
+import java.util.List;
+
+public class AutorService {
+
+    private final AutorRepository repository;
+
+    public AutorService(AutorRepository repository) {
+        this.repository = repository;
+    }
+
+    public AutorModel create(AutorModel autor) {
+        return repository.save(autor);
+    }
+
+    public List<AutorModel> findAll() {
+        return repository.findAll();
+    }
+
+    public AutorModel findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+
+    public void delete(Long id) {
+        AutorModel autor = findById(id);
+        repository.delete(autor);
+    }
+}
