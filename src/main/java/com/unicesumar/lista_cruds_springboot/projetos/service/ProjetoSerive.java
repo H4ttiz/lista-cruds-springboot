229f09089febe6d38baad5454b1f375cc7f10844
H4ttiz <leonardocbizerra@gmail.com>
2026-03-22 00:14:15 -0300

    feat: Adiciona service de Projetos

---
+++ src/main/java/com/unicesumar/lista_cruds_springboot/projetos/service/ProjetoSerive.java
@@ -0,0 +1,35 @@
+package com.unicesumar.lista_cruds_springboot.projetos.service;
+
+import com.unicesumar.lista_cruds_springboot.projetos.model.ProjetoModel;
+import com.unicesumar.lista_cruds_springboot.projetos.repository.ProjetoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjetoSerive {
+
+    private final ProjetoRepository repository;
+
+    public ProjetoSerive(ProjetoRepository repository) {
+        this.repository = repository;
+    }
+
+    public ProjetoModel create(ProjetoModel novoProjeto){
+        return repository.save(novoProjeto);
+    }
+
+    public List<ProjetoModel> findAll(){
+        return repository.findAll();
+    }
+
+    public ProjetoModel findById(Long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Projeto não encontrado"));
+    }
+
+    public void delete(Long id){
+        ProjetoModel dado = findById(id);
+        repository.delete(dado);
+    }
+}
